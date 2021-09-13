9fa3d10261cbe17ab25b9fcb24bff141979caaa7
Remastered Goldfish <99067800@mydavinci.nl>
2021-09-13 10:17:13 +0200

    'sollicitatie'

---
+++ sollicitatie.py
@@ -0,0 +1,8 @@
+print('Dit is de vacature voor ruimtevuilnisman.')
+print('Voor deze functie moet je aan het volgende voldoen')
+
+print('wat zijn uw praktijkervaring? kies uit de volgende 3.')
+dieren_dressuur = int(input('hoelang is je ervaring met dieren-dressuur? '))
+if dieren_dressuur >= 4:
+    print('je heb 4 jaar (of meer) ervaring met dieren dressuur')
+
